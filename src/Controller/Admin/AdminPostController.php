34544219cad2262e0d80c3f007aafa42d332a736
juniorpires <jpires_1@hotmail.com>
2019-07-16 21:36:53 -0300

    CRUD de posts

--- src/Controller/Admin/AdminPostController.php
+++ src/Controller/Admin/AdminPostController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Post;
 use App\Form\PostType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/admin/posts")
@@ -28,12 +29,77 @@ class AdminPostController extends AbstractController
     /**
      * @Route("/new", name="admin_post_new")
      */
-    public function new()
+    public function new(Request $request)
     {
         $form = $this->createForm(PostType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+
+            $post->setCreatedAt(new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")));
+            $post->setUpdatedAt(new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")));
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Post salvo com sucesso');
+            return  $this->redirect('/admin/posts');
+        }
+
 
         return $this->render('admin/posts/new.html.twig', [
             'form' => $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/update/{id}", name="admin_post_update")
+     * @param Request $request
+     * @param Post $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function update(Request $request, Post $id) {
+
+        $form = $this->createForm(PostType::class, $id);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+
+            $post->setUpdatedAt(new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->merge($post);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Post atualizado com sucesso');
+            return  $this->redirect('/admin/posts');
+        }
+
+        return $this->render('admin/posts/update.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="admin_post_delete")
+     */
+    public function delete(Post $post) {
+
+//        if(!$post){
+//            $this->addFlash('warning', 'Post não encontrado !!');
+//            return  $this->redirect('/admin/posts');
+//        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($post);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Post removido com sucesso');
+        return  $this->redirect('/admin/posts');
+    }
+
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -23,6 +24,7 @@ class PostType extends AbstractType
     {
         $resolver->setDefaults([
             // Configure your form options here
+            'data_class' => Post::class,
         ]);
     }
 }
